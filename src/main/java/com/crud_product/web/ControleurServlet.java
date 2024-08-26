6271c1764d4418018c2fe1142b824b749b7da23c
Bonevy BEBY <bonevybeby@gmail.com>
2024-08-27 00:31:24 +0200

    manage search form process

--- src/main/java/com/crud_product/web/ControleurServlet.java
+++ src/main/java/com/crud_product/web/ControleurServlet.java
@@ -1,9 +1,11 @@
 package com.crud_product.web;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.crud_product.dao.IProduit;
 import com.crud_product.dao.ProduitImpl;
+import com.crud_product.metier.entities.Produit;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -17,9 +19,22 @@ public class ControleurServlet extends HttpServlet{
 	public void init() throws ServletException {
 		metier=new ProduitImpl();
 	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("produits.jsp").forward(req, resp);
+		String path=req.getServletPath();
+		if(path.equals("/index.do")) {
+			req.getRequestDispatcher("produits.jsp").forward(req, resp);
+		}
+		else if(path.equals("/chercher.do")) {
+			String motCle=req.getParameter("motCle");
+			ProduitModel model=new ProduitModel();
+			model.setMotCle(motCle);
+			List<Produit>produits=metier.produitsParMC("%"+motCle+"%");
+			model.setProduits(produits);
+			req.setAttribute("model", model);
+			req.getRequestDispatcher("produits.jsp").forward(req, resp);
+		}
 	}
-
+	
 }
--- src/main/webapp/produits.jsp
+++ src/main/webapp/produits.jsp
@@ -4,9 +4,24 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
+<link href="public/bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" />
 <title>Insert title here</title>
 </head>
 <body>
-    Produit
+  <div class="container mt-3 col-md-10 col-md-offset-1">
+  <h3 class="navbar navbar-dark bg-primary" style="color:white;">Recherche des produits</h3>
+  <div class="card">
+    <div class="card-body">
+       <form action="chercher.do" method="get">
+           <label>Mot Clé:</label>
+           <input type="text" name="motCle" />
+            <button type="submit" class="btn btn-primary">Chercher</button>
+       </form>
+    </div>
+
+  <br>
+  </div>
+    
+</div>
 </body>
 </html>
